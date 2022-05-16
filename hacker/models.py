643726a6d9e61b72c1948caf51f62ea0973a40bb
Abhi Vempati <abhijitvempati@gmail.com>
2022-05-15 23:43:18 -0400

    models update

--- hacker/models.py
+++ hacker/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from default.models import CustomUser
+from teams.models import TeamRoster
 # Create your models here.
 
 
 class HackerInfo(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="hacker")
-
+    team = models.ForeignKey(TeamRoster, on_delete=models.CASCADE)
 
     class Meta():
         verbose_name = 'Hacker'
--- teams/models.py
+++ teams/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from default.models import CustomUser
 
 # Create your models here.
 
 class TeamRoster(models.Model):
     name = models.CharField(max_length=120, blank=False, null=False)
     description = models.CharField(max_length=180, blank=False, null=False)
+    leader = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='user')
     is_visible = models.BooleanField()
+
+    def __str__(self):
+        return self.name
