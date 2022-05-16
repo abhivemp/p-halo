472065b31955452cfac63a7e74462ef75b560519
Abhi Vempati <abhijitvempati@gmail.com>
2022-05-16 11:42:40 -0400

    is in team utlity

--- teams/utility.py
+++ teams/utility.py
@@ -7,5 +7,14 @@ from .models import TeamRoster
 from default.models import CustomUser
 
 
-def is_in_team():
+"""
+Anyone in a team cannot create or join a team. 
+This function tests if a user is in a team
+"""
+def is_in_team(user):
+    hacker_object = HackerInfo.objects.get(user = user)
+    return hacker_object.team is not None
+
+
+
     
