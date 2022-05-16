88612b027d4bb7aad73bf661aefddac4159fc29a
Abhi Vempati <abhijitvempati@gmail.com>
2022-05-15 21:29:37 -0400

    teams roster model

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class TeamRoster(models.Model):
+    name = models.CharField(max_length=120, blank=False, null=False)
+    description = models.CharField(max_length=180, blank=False, null=False)
+    is_visible = models.BooleanField()
