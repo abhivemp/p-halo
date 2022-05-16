3c0d96e7d5c47df4812eee49bdce4aadd29ec45e
Abhi Vempati <abhijitvempati@gmail.com>
2022-05-16 16:43:49 -0400

    setup views urls and integrated to existing frontend

--- teams/urls.py
+++ teams/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from teams import views 
 
 urlpatterns = [
-
+    path('team', views.team_dash, name='team-dash'),
 ]
--- teams/views.py
+++ teams/views.py
@@ -3,5 +3,13 @@ from .models import TeamRoster
 from hacker.models import HackerInfo
 from default.models import CustomUser
 
+from .utility import is_in_team
 # Create your views here.
+"""
+Display public teams if the user is not in a team. 
+If the user is in a team, show the user's team info page
+"""
+def team_dash(request):
 
+    context = {}
+    return render(request, 'teams/team-dash.html', context)
