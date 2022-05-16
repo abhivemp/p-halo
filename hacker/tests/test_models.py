c47ec6a3eccc9602d17a02b84edb2cb3fbba2870
Abhi Vempati <abhijitvempati@gmail.com>
2022-05-16 00:24:57 -0400

    added hacker creation test

--- hacker/tests/test_models.py
+++ hacker/tests/test_models.py
@@ -5,4 +5,8 @@ from default.models import CustomUser
 
 
 class HackerInfoTest(TestCase):
-    
+
+    def create_hacker(self, email="limitless@jujutsutech.jp", first_name="gojo", last_name="satoru"):
+        test_user_obj = CustomUser.objects.create(email=email, first_name=first_name, last_name=last_name)
+        return HackerInfo.objects.create(user=test_use_obj)
+        
