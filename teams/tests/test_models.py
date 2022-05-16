550ba25c9f6e6ce89da3ef3314674e393264736d
Abhi Vempati <abhijitvempati@gmail.com>
2022-05-16 15:52:54 -0400

    parameterized test for adding people in different teams

--- teams/tests/test_models.py
+++ teams/tests/test_models.py
@@ -62,3 +62,90 @@ class TeamsRosterTest(TestCase):
     #         test_team_2 = TeamRoster.objects.create(name='kyoto-jujutsu-tech', description='second years at jujutsu sorcery tech', leader=user_leader,is_visible=False)
 
     #     self.assertEqual(str(ue.exception), 'UNIQUE constraint failed')
+
+class TestTeamBuilding(TestCase):
+
+    def setUp(self):
+        emails = [
+            'fushiguro@jujutsutech.jp',
+            'aoi@jujutsutech.jp',
+            'yuta@jujutsutech.jp',
+            'nobara@jujutsutech.jp',
+            'yuji@jujutsutech.jp',
+            'inumaki@jujutsutech.jp',
+            'maki@jujutsutech.jp',
+            'panda@jujutsutech.jp',
+            'mai@jujutsutech.jp',
+            'kasumi@jujutsutech.jp',
+        ]
+        first_names = [
+            'Fushiguro',
+            'Aoi',
+            'Yuta',
+            'Yuji',
+            'Nobara',
+            'Inumaki',
+            'Maki',
+            'Panda',
+            'Mai',
+            'Kasumi'
+        ]
+
+        member_emails = [
+            'nobara@jujutsutech.jp',
+            'panda@jujutsutech.jp',
+            'maki@jujutsutech.jp',
+            'inumaki@jujutsutech.jp',
+            'mai@jujutsutech.jp',
+            'kasumi@jujutsutech.jp',
+            'yuji@jujutsutech.jp',
+        ]
+
+        team_names = [
+            'tokyo',
+            'kyoto',
+            'special-grade',
+        ]
+
+        for i in range (len(emails)):
+            user = CustomUser.objects.create(email=emails[i], first_name=[i])
+            hacker = HackerInfo.objects.create(user=user)
+        
+        for i in range (len(team_names)):
+            team = TeamRoster.objects.create(name=team_names[i], leader=CustomUser.objects.get(email=emails[i]), is_visible=False)
+        
+        
+    def test_multiple_team_members(self):
+        self.fixture_data = (
+            (
+                'nobara@jujutsutech.jp','tokyo',
+            ),
+            (
+                'panda@jujutsutech.jp', 'tokyo',
+            ),
+            (
+                'maki@jujutsutech.jp', 'tokyo',
+            ),
+            (
+                'inumaki@jujutsutech.jp', 'tokyo',
+            ),
+            (
+                'mai@jujutsutech.jp', 'kyoto',
+            ),
+            (
+                'kasumi@jujutsutech.jp', 'kyoto',
+            ),
+            (
+                'yuji@jujutsutech.jp', 'special-grade',
+            ),
+        )
+
+        for member, team_name in self.fixture_data:
+            with self.subTest(context=member):
+                hacker = HackerInfo.objects.get(user__email=member)
+                team_obj = TeamRoster.objects.get(name=team_name)
+                hacker.team = team_obj
+                hacker.save()
+
+                check_hacker_team = HackerInfo.objects.get(user__email=member)
+                self.assertEqual(check_hacker_team.team.name, team_name)
